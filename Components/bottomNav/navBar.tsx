6d200a4b9f313b8f3a294502085741135973a67a
luke boat <lucian.boatright@gmail.com>
2022-03-04 16:50:42 +0000

    save state not happy with outcome though

--- Components/bottomNav/navBar.tsx
+++ Components/bottomNav/navBar.tsx
@@ -1,12 +1,33 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import {
+  createBottomTabNavigator,
+  BottomTabBarProps,
+} from '@react-navigation/bottom-tabs';
+import React, { FC } from 'react';
+import { Dashboard, Home } from '../../Screens';
+
+import CustomTabBar from './tabBarLayout';
 
 const Tab = createBottomTabNavigator();
 
-function MyTabs() {
-  return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Dashboard" component={Dashboard} />
-    </Tab.Navigator>
-  );
+const TabNavigator : FC = () => {
+    return (
+        <NavigationContainer>
+            <Tab.Navigator
+                initialRouteName="One"
+                tabBar={({ state, descriptors, navigation }: BottomTabBarProps) =>
+                <CustomTabBar
+                    state={state}
+                    descriptors={descriptors}
+                    navigation={navigation}
+                />
+                }
+            >
+                <Tab.Screen name='Home' component={Home} />
+                <Tab.Screen name='Dash' component={Dashboard} />
+            </Tab.Navigator>
+        </NavigationContainer>
+    )
 }
+
+export default TabNavigator
--- Navigation/AppStack.tsx
+++ Navigation/AppStack.tsx
@@ -1,16 +1,20 @@
 import React, { FC } from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
-import { Home, AuthDashboard } from '../Screens';
+// import { Home, AuthDashboard } from '../Screens';
 
+import TabNavigator from '../Components/bottomNav/navBar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import SafeAreaView from 'react-native-safe-area-view';
 
-const { Navigator, Screen } = createStackNavigator();
+// const { Navigator, Screen } = createStackNavigator();
 
 const AppStack : FC = () => {
     return (
-        <Navigator screenOptions={{headerShown: false}}>
-            <Screen name="Home" component={Home} />
-            <Screen name="AuthDashboard" component={AuthDashboard} />
-        </Navigator>
+        <SafeAreaProvider>
+            <SafeArea>
+                <TabNavigator />
+            </SafeArea>
+        </SafeAreaProvider>
 
     )
 }
